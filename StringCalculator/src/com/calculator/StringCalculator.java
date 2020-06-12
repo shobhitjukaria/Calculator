b16dd8143e048b1675f4faf7902679112f69b46b
91989 <91989@DESKTOP-AFBDKVU>
2020-06-13 01:34:07 +0530

    To check for negatives ang print negatives list

--- StringCalculator/src/com/calculator/StringCalculator.java
+++ StringCalculator/src/com/calculator/StringCalculator.java
@@ -5,23 +5,52 @@ public class StringCalculator {
 	public int add(String inputString) {
 		if (inputString.isEmpty())
 			return 0;
-		String delimiter = ",|\n";
-		String newString=inputString;
-		if(inputString.startsWith("//")) {
-			delimiter=inputString.substring(2,3);
-			newString=inputString.substring(4);
+		String delimiter = findDelimiter(inputString);
+		String newString = findString(inputString);
+		checkNegatives(findArray(newString, delimiter));
+		return calculateSum(findArray(newString, delimiter));
+	}
+
+	private String findString(String inputString) {
+		String newString = inputString;
+		if (inputString.startsWith("//")) {
+			newString = inputString.substring(4);
 		}
-		String[] stringArray = findArray(newString, delimiter);
-		int sum = calculateSum(stringArray);
-		return sum;
+		return newString;
+	}
+
+	private String findDelimiter(String inputString) {
+		String delimiter = ",|\n";
+		if (inputString.startsWith("//")) {
+			delimiter = inputString.substring(2, 3);
+		}
+		return delimiter;
 	}
 
 	private int calculateSum(String[] stringArray) {
 		int total = 0;
 		for (String num : stringArray) {
-			total += Integer.valueOf(num);
+			total += toInt(num);
 		}
 		return total;
+	}
+
+	private void checkNegatives(String[] stringArray) {
+		String negatives = "";
+		for (String val : stringArray) {
+			if (toInt(val) < 0) {
+				negatives += "," + val;
+			}
+		}
+		if (negatives.length() > 0) {
+			throw new RuntimeException("Negatives Not Allowed! "+negatives.substring(1));
+		}
+
+	}
+
+	private int toInt(String num) {
+		Integer number = Integer.valueOf(num);
+		return number;
 	}
 
 	private String[] findArray(String inputString, String delimiter) {
--- StringCalculator/src/com/calculator/calculatorTest/StringCalculatorTest.java
+++ StringCalculator/src/com/calculator/calculatorTest/StringCalculatorTest.java
@@ -1,6 +1,7 @@
 package com.calculator.calculatorTest;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
@@ -9,29 +10,39 @@ import com.calculator.StringCalculator;
 public class StringCalculatorTest {
 
 	StringCalculator calculator = new StringCalculator();
-	
+
 	@Test
 	public void shouldReturnZeroOnEmptyString() {
-		assertEquals(0,calculator.add(""));
+		assertEquals(0, calculator.add(""));
 	}
-	
+
 	@Test
 	public void shouldReturnSingleNumber() {
-		assertEquals(1,calculator.add("1"));
+		assertEquals(1, calculator.add("1"));
 	}
-	
+
 	@Test
 	public void shouldReturnSumOfNumbersSeparatedByCommas() {
-		assertEquals(5,calculator.add("2,3"));
+		assertEquals(5, calculator.add("2,3"));
 	}
-	
+
 	@Test
 	public void shouldReturnSumOfNumbersSeparatedByNewLineDelimiter() {
-		assertEquals(9,calculator.add("2\n3,4"));
+		assertEquals(9, calculator.add("2\n3,4"));
 	}
-	
+
 	@Test
 	public void shouldReturnSumOfNumbersSeparatedByCustomDelimiter() {
-		assertEquals(7,calculator.add("//;\n1;6"));
+		assertEquals(7, calculator.add("//;\n1;6"));
+	}
+
+	@Test
+	public void checkForNegativesAndPrintNegativesList() {
+		try {
+			calculator.add("-1,-2,2,-3");
+			fail();
+		}catch(Exception e) {
+			assertEquals("Negatives Not Allowed! -1,-2,-3",e.getMessage());
+		}
 	}
 }
